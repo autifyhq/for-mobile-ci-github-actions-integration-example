bb07100f4e17a62122a2547a026403d8aa073b20
Sam Yamashita <1587053+sotayamashita@users.noreply.github.com>
2021-09-03 14:22:25 +0900

    Add AwesomeProject-iOS

---
+++ AwesomeProject-iOS/AwesomeProject_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  AwesomeProject_iOSApp.swift
+//  AwesomeProject-iOS
+//
+//  Created by Sota Yamashita on 2021/09/03.
+//
+
+import SwiftUI
+
+@main
+struct AwesomeProject_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AwesomeProject-iOS/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AwesomeProject-iOS
+//
+//  Created by Sota Yamashita on 2021/09/03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
